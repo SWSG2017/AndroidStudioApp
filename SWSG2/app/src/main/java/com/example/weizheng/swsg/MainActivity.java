e7b48bb5bbadbbcbefc0b65ac1d98270acf74b12
fongwz <schifx95@gmail.com>
2017-09-30 04:41:07 +0800

    qr scanner+initial http

--- SWSG2/app/src/main/java/com/example/weizheng/swsg/MainActivity.java
+++ SWSG2/app/src/main/java/com/example/weizheng/swsg/MainActivity.java
@@ -1,13 +1,201 @@
 package com.example.weizheng.swsg;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.core.IViewFinder;
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+import static android.Manifest.permission.CAMERA;
+
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler{
+
+    /**
+     * ZXing and camera variables
+     */
+    private static final int REQUEST_CAMERA = 1;
+    private ZXingScannerView scannerView;
+
+    /**
+     * HTTP request variables
+     */
+    private static final String httpTAG = "HTTP";
+    private String url = "192.168.137.1/server/menus/0000";
+    private RequestQueue mRequestQueue;
+    private StringRequest mStringRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        scannerView = new ZXingScannerView(this) {
+            @Override
+            protected IViewFinder createViewFinderView(Context context){
+                return new CustomZXingScannerView(context);
+            }
+        }; //Initialize custom scannerView
+
         setContentView(R.layout.activity_main);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(checkPermission()) {
+
+                Toast.makeText(MainActivity.this, "Permission is granted", Toast.LENGTH_SHORT).show();
+
+            } else {
+                requestPermission();
+            }
+        }
+    }
+
+
+    public void startScan(View view) {
+        setContentView(scannerView);
+    }
+
+    /**
+     * Logic to handle scan results
+     */
+    @Override
+    public void handleResult(Result result) {
+        /**
+         * For a string result
+         */
+        final String scanResult = result.getText();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Scan Result");
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                scannerView.resumeCameraPreview(MainActivity.this);
+            }
+        });
+        builder.setNeutralButton("Show Message", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, scanResult, Toast.LENGTH_SHORT).show();
+                //send request and print response using volley library
+                sendRequest(scanResult);
+            }
+        });
+        builder.setMessage("QR code retrieved");
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    private void sendRequest(String scanResult) {
+
+        //url += "/" + scanResult;
+        Log.i(httpTAG, url);
+        mRequestQueue = Volley.newRequestQueue(this);
+        mStringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.i(httpTAG, "Response: " + response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i(httpTAG, "Error: " + error.toString());
+            }
+        });
+
+        mRequestQueue.add(mStringRequest); //RequestQueue fires stringRequest as soon as its added
+    }
+
+    /**
+     * Camera housekeeping
+     * Contains list of methods to check for permission and launch camera to scan for QR code
+     */
+    private boolean checkPermission(){
+        return (ContextCompat.checkSelfPermission(MainActivity.this, CAMERA) == PackageManager.PERMISSION_GRANTED);
+    }
+
+    private void requestPermission(){
+        ActivityCompat.requestPermissions(this, new String[]{CAMERA}, REQUEST_CAMERA);
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String permission[], int grantResults[]){
+        switch(requestCode)
+        {
+            case REQUEST_CAMERA:
+                if(grantResults.length>0) {
+                    boolean cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    if(cameraAccepted){
+                        Toast.makeText(MainActivity.this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            if(shouldShowRequestPermissionRationale(CAMERA)){
+                                displayAlertMessage("You need to allow access for both permissions",
+                                        new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                    requestPermissions(new String[]{CAMERA} , REQUEST_CAMERA);
+                                                }
+                                            }
+                                        });
+                                return;
+                            }
+                        }
+                    }
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(checkPermission()){
+                if(scannerView==null) {
+                    scannerView = new ZXingScannerView(this);
+                    setContentView(scannerView);
+                }
+                scannerView.setResultHandler(this);
+                scannerView.startCamera();
+            } else {
+                requestPermission();
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+
+        scannerView.stopCamera();
+    }
+
+    public void displayAlertMessage(String message, DialogInterface.OnClickListener listener){
+        new AlertDialog.Builder(MainActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", listener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
     }
 }
